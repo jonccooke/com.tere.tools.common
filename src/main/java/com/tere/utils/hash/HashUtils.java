3e87acf70e5f55f485b4f5ad0c92cb98d73abb1f
Jonc <Jonc@ENIGMA>
2018-08-14 07:54:17 +0100

    added array method

--- src/main/java/com/tere/utils/hash/HashUtils.java
+++ src/main/java/com/tere/utils/hash/HashUtils.java
@@ -220,4 +220,15 @@ public class HashUtils
 		return value.hashCode();
 	}
 
+    public static int hashCode(int a[]) {
+        if (a == null)
+            return 0;
+
+        int result = 1;
+        for (int element : a)
+            result = 31 * result + element;
+
+        return result;
+    }
+
 }
